d176e0407368f22ece4275d2359b76d30951bb84
pGUPT4 <pratbro1@gmail.com>
2024-10-20 12:35:48 -0700

    CSS designing finished

---
+++ my-app/app/component/feedTray.js
@@ -0,0 +1,11 @@
+'use client'
+
+export default function FeedTray() {
+    return(
+        <div className="flex justify-center">
+            <div class="mt-20 w-[1100px] h-[700px] bg-gray-400 rounded-lg">
+                FeedTray
+            </div>
+        </div>
+    )
+}
--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,9 +1,19 @@
 'use client'
 
+import Navbar from "./component/navbar";
+import FeedTray from "./component/feedTray";
+
 export default function Home() {
 	return (
 		<div>
-			
+			<Navbar>
+
+			</Navbar>
+
+
+			<FeedTray>
+				
+			</FeedTray>
 		</div>
 	);
 }
